f4e3ade1de36627b00b4596e17e346c7d67c7f59
omjadas <omja.das@datatrue.com>
2019-12-18 11:36:51 +1100

    fix hook when there are no unstaged / tracked changes

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -3,7 +3,7 @@
 set -e
 
 git restore docs
-git commit -n --allow-empty --message "temp"
+git commit --allow-empty --no-verify --message "temp"
 git stash --include-untracked
 git reset --soft HEAD^
 
@@ -11,4 +11,8 @@ if git diff --name-only --cached | grep -q "^src/"; then
   npx typedoc
 fi
 
+set +e
+
 git stash pop
+
+exit 0
