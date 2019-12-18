c41ee6a55a45a1a5d616f971a3d18c30f2af5028
omjadas <omja.das@datatrue.com>
2019-12-18 12:00:47 +1100

    better output for docs script

--- scripts/docs.sh
+++ scripts/docs.sh
@@ -2,17 +2,19 @@
 
 set -e
 
-git restore docs
-git commit --allow-empty --no-verify --message "temp"
-git stash --include-untracked
-git reset --soft HEAD^
+git restore docs --quiet
+git commit --allow-empty --no-verify --quiet --message "temp"
+git stash --include-untracked --quiet
+git reset --quiet --soft HEAD^
 
 if git diff --name-only --cached | grep -q "^src/"; then
   npx typedoc
+else
+  echo "No staged changes to src, docs will not be generated"
 fi
 
 set +e
 
-git stash pop
+git stash pop > /dev/null 2>&1
 
 exit 0
