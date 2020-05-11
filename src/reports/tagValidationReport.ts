aed39678e1c8739da5fee01c02e4493c32f3a4b2
omjadas <omja.das@datatrue.com>
2020-05-12 08:35:19 +1000

    chore: add tag validation dimensions and metrics

--- src/reports/tagValidationReport.ts
+++ src/reports/tagValidationReport.ts
@@ -1,11 +1,30 @@
 import { ExtractSetGeneric, Report } from "./report";
+import { testResultDimensions } from "./testResultReport";
 
-export const tagValidationDimensions = new Set([] as const);
+export const tagValidationDimensions = new Set(
+  [
+    ...[
+      "process_time",
+      "process_date",
+      "step_id",
+      "tag_definition_id",
+      "page_url",
+      "step_name",
+      "tag_definition_name",
+      "tag_type",
+      "tag_validation_status",
+      "tag_vendor",
+    ] as const,
+    ...testResultDimensions,
+  ]
+);
 export type TagValidationDimension = ExtractSetGeneric<
   typeof tagValidationDimensions
 >;
 
-export const tagValidationMetrics = new Set([] as const);
+export const tagValidationMetrics = new Set([
+  "count_tag_validations",
+] as const);
 export type TagValidationMetric = ExtractSetGeneric<
   typeof tagValidationMetrics
 >;
