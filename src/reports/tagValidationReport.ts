27882d60bbe2ee88d51d7379a1cb317dbb53b356
omjadas <omja.das@datatrue.com>
2020-05-12 08:40:58 +1000

    fix: remove dimensions not implemented for tag validations

--- src/reports/tagValidationReport.ts
+++ src/reports/tagValidationReport.ts
@@ -1,22 +1,40 @@
 import { ExtractSetGeneric, Report } from "./report";
-import { testResultDimensions } from "./testResultReport";
 
 export const tagValidationDimensions = new Set(
   [
-    ...[
-      "process_time",
-      "process_date",
-      "step_id",
-      "tag_definition_id",
-      "page_url",
-      "step_name",
-      "tag_definition_name",
-      "tag_type",
-      "tag_validation_status",
-      "tag_vendor",
-    ] as const,
-    ...testResultDimensions,
-  ]
+    "process_time",
+    "process_date",
+    "step_id",
+    "tag_definition_id",
+    "page_url",
+    "step_name",
+    "tag_definition_name",
+    "tag_type",
+    "tag_validation_status",
+    "tag_vendor",
+    "start_date",
+    "app_name",
+    "app_version",
+    "draft_tag_name",
+    "draft_tag_status",
+    "browser",
+    "browser_version",
+    "device_name",
+    "os",
+    "os_version",
+    "platform",
+    "platform_version",
+    "initiated_by",
+    "region_id",
+    "region_name",
+    "status",
+    "suite_name",
+    "suite_type",
+    "test_run_mode",
+    "test_run_type",
+    "test_scenario_name",
+    "test_type",
+  ] as const,
 );
 export type TagValidationDimension = ExtractSetGeneric<
   typeof tagValidationDimensions
