dc039b44e0d03f98ac9d589c00461e998bc857c7
omjadas <omja.das@datatrue.com>
2020-05-15 10:55:24 +1000

    chore: update samples

--- samples/datatrue-solution-design/src/deleteTokens.ts
+++ samples/datatrue-solution-design/src/deleteTokens.ts
@@ -1,5 +1,4 @@
 export function deleteTokens(): void {
   const userProperties = PropertiesService.getUserProperties();
   userProperties.deleteProperty("DATATRUE_USER_TOKEN");
-  userProperties.deleteProperty("DATATRUE_ACCOUNT_TOKEN");
 }
--- samples/datatrue-solution-design/src/setTokens.ts
+++ samples/datatrue-solution-design/src/setTokens.ts
@@ -5,11 +5,7 @@ export function setTokens(): void {
   const userTokenPrompt = ui.prompt("Please enter your API tokens", "User API Key (for creating tests)", ui.ButtonSet.OK);
   const userToken = userTokenPrompt.getResponseText();
 
-  const accountTokenPrompt = ui.prompt("Please enter your API tokens", "Account API Key (for running tests)", ui.ButtonSet.OK);
-  const accountToken = accountTokenPrompt.getResponseText();
-
   userProperties.setProperties({
     "DATATRUE_USER_TOKEN": userToken,
-    "DATATRUE_ACCOUNT_TOKEN": accountToken,
   });
 }
