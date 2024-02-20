2adc58acecf9f526a5c05e8b5c4ffd9650f63fe1
omjadas <omja.das@datatrue.com>
2024-02-20 12:53:44 +1100

    refactor: use Object.assign

--- samples/datatrue-solution-design/src/index.ts
+++ samples/datatrue-solution-design/src/index.ts
@@ -4,13 +4,10 @@ import { onOpen } from "./menu";
 import { run } from "./run";
 import { setTokens } from "./setTokens";
 
-// @ts-ignore
-global.create = create;
-// @ts-ignore
-global.deleteTokens = deleteTokens;
-// @ts-ignore
-global.onOpen = onOpen;
-// @ts-ignore
-global.run = run;
-// @ts-ignore
-global.setTokens = setTokens;
+Object.assign(globalThis, {
+  create,
+  deleteTokens,
+  onOpen,
+  run,
+  setTokens,
+})
